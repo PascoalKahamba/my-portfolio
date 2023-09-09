452d6e91e50d2ef4ebec817c734cd7d432144f02
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-09-09 12:43:54 +0100

    I'm improving the code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,7 +41,6 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Pascoal Kahamba</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
