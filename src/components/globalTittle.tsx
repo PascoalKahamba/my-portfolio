b4e005dd7e7e67b3afd9545ad8fc1f3de3287bbd
pascoalkahamba <pascoalkahamba25@gmail.com>
2023-09-20 09:34:46 +0100

    I've created the globalTitle component

---
+++ src/components/globalTittle.tsx
@@ -0,0 +1,40 @@
+import { createStyles, rem } from "@mantine/core";
+import React from "react";
+
+const useStyles = createStyles((theme) => ({
+  line: {
+    height: rem(3),
+    // flex: " 1 1 40rem",
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+  },
+}));
+
+interface GlobalTitleProps {
+  title: string;
+  width: number;
+  leftWidth: number;
+}
+
+export default function GlobalTitle({ title, width }: GlobalTitleProps) {
+  const { classes } = useStyles();
+  return (
+    <>
+      <div
+        className={classes.line}
+        style={
+          title === "FRONT-END" ? { width: rem(width) } : { width: rem(width) }
+        }
+      ></div>
+      <h1>{title}</h1>
+      <div
+        className={classes.line}
+        style={
+          title === "FRONT-END" ? { width: rem(width) } : { width: rem(width) }
+        }
+      ></div>
+    </>
+  );
+}
